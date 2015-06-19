0327b910f75eacad53e1c9c80a33505172baa086
danieloprado <danieloprado@outlook.com>
2015-06-19 12:02:54 -0300

    gorilla file

---
+++ src/gorilla.utilities.file.js
@@ -0,0 +1,26 @@
+(function ($u, $) {
+    "use strict";
+
+    var self = $u.file = {};
+
+    self.getExtension = function (filename) {
+        var pos = filename.lastIndexOf(".");
+        return filename.substr(pos, filename.length - pos);
+    }
+
+    self.hasExtension = function (filename, extensions) {
+        var result = false;
+        $.each(extensions, function (key, ext) {
+            var reg = new RegExp("[\\s\\S]+\\" + ext + "\\b");
+
+            if (reg.test(filename)) {
+                result = true;
+                return false;
+            }
+        });
+
+        return result;
+    }
+
+
+})(window.gorilla = window.gorilla || {}, jQuery);
