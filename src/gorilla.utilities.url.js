7abab1a5573e13b2ae45617b35234506a6b29a5d
danieloprado <danieloprado@outlook.com>
2015-07-10 09:46:20 -0300

    url: videoInfo now return the iframe url

--- src/gorilla.utilities.url.js
+++ src/gorilla.utilities.url.js
@@ -1,43 +1,46 @@
-(function ($u) {
-    "use strict";
+(function($u) {
+  "use strict";
 
-    var self = $u.url = {};
+  var self = $u.url = {};
 
-    self.youtube = function(url) {
-    	var code = url.match($u.regexp.youtube);
-		if(!code){
-			return null;
-		}
+  self.youtube = function(url) {
+    var code = url.match($u.regexp.youtube);
+    if (!code) {
+      return null;
+    }
 
-		return code[1];
-	};
+    return code[1];
+  };
 
-    self.vimeo = function(url) {
-    	var code = url.match($u.regexp.vimeo);
-		if(!code){
-			return null;
-		}
+  self.vimeo = function(url) {
+    var code = url.match($u.regexp.vimeo);
+    if (!code) {
+      return null;
+    }
 
-		return code[1];
+    return code[1];
+  };
+
+  self.videoInfo = function(videoUrl) {
+    var site = "youtube";
+    var code = self.youtube(videoUrl);
+    var url = 'https://www.youtube.com/embed/' + code;
+
+    if (!code) {
+      site = "vimeo";
+      code = self.vimeo(videoUrl);
+      url = 'https://player.vimeo.com/video/' + code;
+    }
+
+    if (!code) {
+      return false;
+    }
+
+    return {
+      provider: site,
+      code: code,
+      iframeUrl: url
     };
-
-    self.videoInfo = function(url){
-    	var site = "youtube";
-    	var code = self.youtube(url);
-
-    	if(!code){
-    		site = "vimeo";
-    		code = self.vimeo(url);
-    	}
-
-    	if(!code){
-    		return false;
-    	}
-
-    	return {
-    		provider: site,
-    		code: code
-    	};
-    };
+  };
 
 })(window.gorilla = window.gorilla || {});
